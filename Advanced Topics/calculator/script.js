71f7e9b85eac0e2b3b0e574c18a8dac7ebcb8cea
John Fu <fu.johnyh@gmail.com>
2022-08-20 18:36:07 -0700

    calculator assignment

--- Advanced Topics/calculator/script.js
+++ Advanced Topics/calculator/script.js
@@ -4,19 +4,19 @@ function press(btn) {
     if (btn != '.') {
         if (displayValue === '0') {
             displayValue = btn.toString();
-            storedValue = parseFloat(displayValue);
+            // storedValue = parseFloat(displayValue);
             console.log(displayValue);
             console.log(storedValue);
         } else {
             displayValue += btn.toString();
-            storedValue = parseFloat(displayValue);
+            // storedValue = parseFloat(displayValue);
             console.log(displayValue);
             console.log(storedValue);
         }
     } else {
         if (!(displayValue.includes('.'))) {
             displayValue += btn.toString();
-            storedValue = parseFloat(displayValue + 0);
+            // storedValue = parseFloat(displayValue + 0);
             console.log(displayValue);
             console.log(storedValue);
         }
