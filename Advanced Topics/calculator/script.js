6dd15df4d98d1f115d8bd0797969e5f634764a43
John Fu <fu.johnyh@gmail.com>
2022-08-20 21:21:00 -0700

    calculator updated

--- Advanced Topics/calculator/script.js
+++ Advanced Topics/calculator/script.js
@@ -1,5 +1,5 @@
-var displayValue = '0'
-var storedValue = 0
+var displayValue = '0';
+var storedValue = 0;
 function press(btn) {
     if (btn != '.') {
         if (displayValue === '0') {
@@ -16,7 +16,6 @@ function press(btn) {
     } else {
         if (!(displayValue.includes('.'))) {
             displayValue += btn.toString();
-            // storedValue = parseFloat(displayValue + 0);
             console.log(displayValue);
             console.log(storedValue);
         }
@@ -24,11 +23,26 @@ function press(btn) {
 }
 
 function setOP(symbol) {
-
+    if (symbol == '/') {
+        if (parseFloat(displayValue) != 0) {
+            storedValue /= parseFloat(displayValue);
+        }
+    } else if (symbol == '*') {
+        storedValue *= parseFloat(displayValue);
+    } else if (symbol == '+') {
+        storedValue += parseFloat(displayValue);
+    } else if (symbol == '-') {
+        storedValue -= parseFloat(displayValue);
+    }
+    // storedValue = parseFloat(displayValue);
+    displayValue = '0';
+    // console.log(displayValue);
+    // console.log(storedValue);
 }
 
 function clr() {
-
+    displayValue = '0';
+    storedValue = 0;
 }
 
 function calculate() {
